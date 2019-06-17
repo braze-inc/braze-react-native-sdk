1f61bd82d1ef3ecd25a4c86ed168629019043d4f
Alex Didovych <me@alexm.one>
2019-06-17 18:26:51 +0300

    Typings: fixed getContentCards return type

--- index.d.ts
+++ index.d.ts
@@ -451,7 +451,7 @@ export function logContentCardsDisplayed(): void;
  * Returns a content cards array
  * @returns {Promise<ContentCard[]>}
  */
-export function getContentCards(): Promise<ContentCard>;
+export function getContentCards(): Promise<ContentCard[]>;
 // endregion
 
 type MonthsAsNumber = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
