7dd2d3414f36715e8ae8510b37a0c529587456a5
Alex Didovych <me@alexm.one>
2019-06-17 14:21:21 +0300

    Typings: added missing methods

--- index.d.ts
+++ index.d.ts
@@ -47,6 +47,22 @@ export function getInitialURL(callback: (deepLink: string) => void): void;
 export function changeUser(userId: string): void;
 
 /**
+ * An alias serves as an alternative unique user identifier. Use aliases to identify users along different
+ *    dimensions than your core user ID:
+ *       * Set a consistent identifier for analytics that will follow a given user both before and after they have
+ *         logged in to a mobile app or website.
+ *       * Add the identifiers used by a third party vendor to your Braze users in order to more easily reconcile
+ *         your data externally.
+ *
+ * Note: Each alias consists of two parts: a name for the identifier itself, and a label indicating the type of
+ *    alias. Users can have multiple aliases with different labels, but only one name per label.
+ *
+ * @param {string} name - An identifier for alias name.
+ * @param {string} label - An identifier for alias label.
+ */
+export function addAlias(name: string, label: string): void;
+
+/**
  * Sets the first name of the user.
  * @param {string} firstName - Limited to 255 characters in length.
  */
@@ -73,6 +89,12 @@ export function setGender(
   gender: GenderTypes[keyof GenderTypes],
   callback?: Callback
 ): void;
+
+/**
+ * Sets the language for the user.
+ * @param {string} language - Should be valid ISO 639-1 language code.
+ */
+export function setLanguage(language: string): void;
 
 /**
  * Sets the country for the user.
